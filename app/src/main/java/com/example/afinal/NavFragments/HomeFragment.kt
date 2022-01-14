9c1b8f0c355f880d64312d141836b15751502fb4
tornike11 <tornike.elkanishvili.1@btu.edu.ge>
2022-01-14 18:39:26 +0400

    recycler view davamatee #tornike

--- app/src/main/java/com/example/afinal/NavFragments/HomeFragment.kt
+++ app/src/main/java/com/example/afinal/NavFragments/HomeFragment.kt
@@ -1,7 +1,52 @@
 package com.example.afinal.NavFragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.afinal.R
+import com.example.myapplication.Posts
+import com.example.myapplication.RecyclerViewPersonAdapter
 
 class HomeFragment: Fragment(R.layout.fragment_home) {
+        private lateinit var recyclerView: RecyclerView
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            recyclerView = view.findViewById(R.id.recyclerView)
+            recyclerView.layoutManager = LinearLayoutManager(requireContext())
+            recyclerView.adapter = RecyclerViewPersonAdapter(getData())
+        }
+
+        private fun getData(): List<Posts> {
+            val list = ArrayList<Posts>()
+            list.add(
+                Posts(
+                    1,
+                    "https://www.myanbiz-consulting.com/wp-content/uploads/2019/04/4214892-news-images.jpg",
+                    "Best news in the world\nfor you"
+                )
+            )
+            list.add(
+                Posts(
+                    2,
+                    "https://www.myanbiz-consulting.com/wp-content/uploads/2019/04/4214892-news-images.jpg",
+                    "very very gooood\nnews for you"
+                )
+            )
+            list.add(
+                Posts(
+                    3,
+                    "https://www.myanbiz-consulting.com/wp-content/uploads/2019/04/4214892-news-images.jpg",
+                    "very very gooood\nnews for you"
+                )
+            )
+            list.add(
+                Posts(
+                    4,
+                    "https://www.myanbiz-consulting.com/wp-content/uploads/2019/04/4214892-news-images.jpg",
+                    "very very gooood\nnews for you"
+                )
+            )
+            return list
+        }
 }
